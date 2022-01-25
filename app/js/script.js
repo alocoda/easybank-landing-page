a37c4b61c18c8304add296ac25751e9e5b0e5c9a
alocoda <aloysiusddev5@pm.me>
2022-01-26 01:08:43 +0700

    Add overlay transition.

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,17 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
-  console.log("open hamburger");
-  btnHamburger.classList.toggle('open')
+  if (header.classList.contains("open")) {
+    // Close hamburger menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    // Open hamburger menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
 });
